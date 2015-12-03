80287f2ab45aba8880d481d131c89dc3337431e9
Aron Culotta <aronwc@gmail.com>
2015-12-03 01:07:48 -0600

    doc

--- example/assignment1.py
+++ example/assignment1.py
@@ -1,4 +1,6 @@
-import re
+"""
+A simple example of a student's assignment.
+"""
 
 def is_mammal(text):
     return text in ['cat', 'dog']
--- example/test_assignment1.py
+++ example/test_assignment1.py
@@ -1,5 +1,13 @@
 """
+A simple example of testing the solution to assignment1.py.
+From this directory, you can run:
+  pygrade grade -s students.tsv -t test_assignment1.py
+
+This will generate grades.json.
+
+The file name is relative to each student's github repo:
 @name=example/assignment1.py
+Here we list the total possible points for the assignment:
 @possible_points=5
 """
 import unittest
@@ -8,13 +16,16 @@ from assignment1 import *
 
 class TestAssignment1(unittest.TestCase):
     def test_simple_1(self):
-        """ @points 2 """
+        """
+        We specify the point value for each test in the method comment string:
+        @points=2
+        """
         self.assertTrue(is_mammal('cat'))
 
     def test_simple_2(self):
-        """ @points 2 """
+        """ @points=2 """
         self.assertTrue(is_mammal('dog'))
 
     def test_hard(self):
-        """ @points 1 """
+        """ @points=1 """
         self.assertTrue(is_mammal('dolphin'))
