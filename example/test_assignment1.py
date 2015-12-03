722d949484bc7fe11dc661cd0f98b4c722738b2a
Aron Culotta <aronwc@gmail.com>
2015-12-03 01:04:12 -0600

    fix indents

--- example/test_assignment1.py
+++ example/test_assignment1.py
@@ -8,13 +8,13 @@ from assignment1 import *
 
 class TestAssignment1(unittest.TestCase):
     def test_simple_1(self):
-    """ @points 2 """
-    self.assertTrue(is_mammal('cat'))
+        """ @points 2 """
+        self.assertTrue(is_mammal('cat'))
 
     def test_simple_2(self):
-    """ @points 2 """
-    self.assertTrue(is_mammal('dog'))
+        """ @points 2 """
+        self.assertTrue(is_mammal('dog'))
 
     def test_hard(self):
-    """ @points 1 """
-    self.assertTrue(is_mammal('dolphin'))
+        """ @points 1 """
+        self.assertTrue(is_mammal('dolphin'))
