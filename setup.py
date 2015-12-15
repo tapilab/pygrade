45d8e20774ddf3c30acbab6faa0d863fedda0b14
Aron Culotta <aronwc@gmail.com>
2015-12-15 09:56:23 -0600

    read reqs

--- setup.py
+++ setup.py
@@ -14,16 +14,9 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
-requirements = [
-    # TODO: put package requirements here
-    'docopt',
-    'gitpython',
-    'nose',
-    'numpy',
-    'scipy',
-    'scikit-learn',
-    'wheel',
-]
+# read requirements
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 test_requirements = requirements
 #[
