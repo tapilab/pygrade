18185dc81d79a0bc8f13bb15687f9772b6adef48
Aron Culotta <aronwc@gmail.com>
2016-08-31 15:55:22 -0500

    bump version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = requirements
 
 setup(
     name='pygrade',
-    version='0.2.0',
+    version='0.2.1',
     description="auto-grade python assignments",
     long_description=readme + '\n\n' + history,
     author="Aron Culotta",
